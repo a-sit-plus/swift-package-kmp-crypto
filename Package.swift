1c852b11d932c7affc830dc0c400d6a783e5df1c
Christian Kollmann <christian.kollmann@a-sit.at>
2023-10-30 12:27:10 +0100

    Prepare for next release

--- Package.swift
+++ Package.swift
@@ -10,12 +10,30 @@ let package = Package(
     .library(
       name: "KmpCryptoDatatypes",
       targets: ["KmpCryptoDatatypes"]
+    ),
+    .library(
+      name: "KmpCryptoDatatypesJws",
+      targets: ["KmpCryptoDatatypesJws"]
+    ),
+    .library(
+      name: "KmpCryptoDatatypesCose",
+      targets: ["KmpCryptoDatatypesCose"]
     )
   ],
   targets: [
     .binaryTarget(
       name: "KmpCryptoDatatypes",
       url: "https://github.com/a-sit-plus/kmp-crypto/releases/download/1.0.0/Datatypes-release.xcframework.zip",
+      checksum: "2a1f7b0d24557bb915d40b5de7fd212bb8d193928956ab2369533ab995aaf6be"
+    ),
+    .binaryTarget(
+      name: "KmpCryptoDatatypesJws",
+      url: "https://github.com/a-sit-plus/kmp-crypto/releases/download/1.0.0/DatatypesJws-release.xcframework.zip",
+      checksum: "2a1f7b0d24557bb915d40b5de7fd212bb8d193928956ab2369533ab995aaf6be"
+    ),
+    .binaryTarget(
+      name: "KmpCryptoDatatypesCose",
+      url: "https://github.com/a-sit-plus/kmp-crypto/releases/download/1.0.0/DatatypesCose-release.xcframework.zip",
       checksum: "2a1f7b0d24557bb915d40b5de7fd212bb8d193928956ab2369533ab995aaf6be"
     )
   ]
